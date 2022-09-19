fe3cce90668749c686cf3be43e31e6d659a8921f
eagarcia31 <eagarcia31@miners.utep.edu>
2022-09-19 17:14:46 -0600

    Added files

--- src/README.md
+++ src/README.md
@@ -1,4 +1,5 @@
 Project 1: Tokenizer
+
 ====================
 # Instructions:
 
---
+++ src/tokenizer.c
@@ -0,0 +1,8 @@
+#include <studio.h>
+#include <stdlib.h>
+#include "tokenizer.h"
+#include "history.h"
+
+int space_char(char c){
+  return isspace(c); 
+}
